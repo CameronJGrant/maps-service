b05a4d56597d258ef43bb1102acff72144c44318
Quaternions <krakow20@gmail.com>
2024-11-26 15:55:50 -0800

    modify Submission struct

--- pkg/model/submission.go
+++ pkg/model/submission.go
@@ -3,9 +3,15 @@ package model
 import "time"
 
 type Submission struct {
-	ID          int64
-	DisplayName string
-	Creator     string
-	GameID      int32
-	Date        time.Time
+	ID             int64
+	DisplayName    string
+	Creator        string
+	GameID         int32
+	Date           time.Time
+	Submitter      int64 // UserID
+	AssetID        int64
+	AssetVersion   int64
+	Completed      bool
+	TargetAssetID  int64 // where to upload map fix.  if the TargetAssetID is 0, it's a new map.
+	StatusID       int32
 }
