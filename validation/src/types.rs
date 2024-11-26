666751e2e41a3c3f6c823421184b6361a0b8e298
Quaternions <krakow20@gmail.com>
2024-11-25 20:27:58 -0800

    idiot

--- validation/src/types.rs
+++ validation/src/types.rs
@@ -2,7 +2,7 @@ bitflags::bitflags!{
 	#[derive(Clone,Copy,Debug,Default)]
 	pub struct Games:u32{
 		const Bhop=1<<0;
-		const Surf=2<<0;
-		const FlyTrials=5<<0;
+		const Surf=1<<1;
+		const FlyTrials=1<<4;
 	}
 }
