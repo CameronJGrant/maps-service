1a48a4fb5a7b948724a98e2c29a0f686d0f6324a
Quaternions <krakow20@gmail.com>
2024-12-05 16:16:32 -0800

    tweak policy

--- pkg/model/policy.go
+++ pkg/model/policy.go
@@ -14,8 +14,10 @@ type ScriptPolicy struct {
 	// Hash of the source code that leads to this policy.
 	// If this is a replacement mapping, the original source may not be pointed to by any policy.
 	// The original source should still exist in the scripts table, which can be located by the same hash.
-	Hash           uint64
-	// The ID of the replacement source (ScriptPolicyReplace) or verbatim source (other)
-	ScriptID       int64
+	FromScriptHash uint64
+	// The ID of the replacement source (ScriptPolicyReplace)
+	// or verbatim source (ScriptPolicyAllowed)
+	// or 0 (other)
+	ToScriptID     int64
 	Policy         Policy
 }
